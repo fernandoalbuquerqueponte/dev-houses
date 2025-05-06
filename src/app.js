82b556b441126f0ab10d417f0cff5ab44d5a35cb
Fernando Albuquerque <fernandonetodeveloper@gmail.com>
2025-05-06 15:43:07 -0300

    feat: add initial user model and user controller create user

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-//fazer todas as configurações e configurar nosso express - no backend faz mais sentido a gente utilizar em formato de classe
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,11 +7,9 @@ import mongoose from "mongoose";
 import routes from "./routes";
 
 class App {
-  //metodo construtor é chamado automaticamente quando essa classe é instanciada
   constructor() {
     this.server = express();
 
-    //Configurações para conectar no nosso banco de dados:
     mongoose.connect(process.env.MONGODB_URI);
 
     this.middlewares();
--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-//configurar o nosso servidor
-
 import app from "./app";
 
 app.listen(3333);
