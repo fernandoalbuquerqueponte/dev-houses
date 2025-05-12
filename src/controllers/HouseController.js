7b57eed97dc6046ff559003349e280f9945ff7a3
Fernando Albuquerque <fernandonetodeveloper@gmail.com>
2025-05-12 16:21:29 -0300

    feat: add editing of house and filter houses dashboard

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,4 +1,6 @@
 import House from "../models/House";
+
+import User from "../models/User";
 class HouseController {
   async index(req, res) {
     const { status } = req.query;
@@ -25,6 +27,48 @@ class HouseController {
     });
 
     return res.json(house);
+  }
+
+  async update(req, res) {
+    const { filename } = req.file;
+    const { house_id } = req.params;
+    const { description, price, location, status } = req.body;
+
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+    const houses = await House.findById(house_id);
+
+    if (String(user._id) !== String(houses.user)) {
+      return res.status(401).json({ error: "Não autorizado!" });
+    }
+
+    await House.updateOne(
+      { _id: house_id },
+      {
+        user: user_id,
+        thumbnail: filename,
+        description,
+        price,
+        location,
+        status,
+      }
+    );
+
+    return res.send();
+  }
+
+  async destroy(req, res) {
+    const { house_id } = req.body;
+    const { user_id } = req.headers;
+
+    if (String(user._id) !== String(houses.user)) {
+      return res.status(401).json({ error: "Não autorizado!" });
+    }
+
+    await House.findByIdAndDelete({ _id: house_id });
+
+    return res.json({ message: "excluida com sucesso!" });
   }
 }
 
